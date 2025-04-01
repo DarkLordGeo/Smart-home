9f33f7c89b6cf3b6187a6701674b0248e1b541a2
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-04-01 15:58:18 +0400

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        features: resolve(__dirname, 'features.html'),
+        about: resolve(__dirname, 'about.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        contact: resolve(__dirname, 'contactx.html'),
+      },
+    },
+  },
+})
